c24889926ec9241d6906dc1882bad8049562ff95
iamzaidsarwar_ <82316311+sarwar0001@users.noreply.github.com>
2022-10-08 23:04:11 +0530

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,23 @@
+const contacts = [
+  {
+    name: "Mohd Zaid",
+    imgURL: "https://images.app.google/jgKtVDzdYPHcsegS9",
+    tel: "+91-1234567890",
+    email: "coder@gmail.com"
+  },
+  {
+    name: "Mohd Aakif",
+    imgURL: "https://unsplash.com/photos/o4UhdLv5jbQ",
+    tel: "+91-0987654321",
+    email: "coderx@gmail.com"
+  },
+  {
+    name: "Mohd Hamza",
+    imgURL:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcodersera.com%2Fblog%2Fwp-content%2Fuploads%2F2019%2F07%2FBLOG-23-L-3.jpg&imgrefurl=https%3A%2F%2Fcodersera.com%2Fblog%2F10-steps-to-become-a-top-coder%2F&tbnid=hEWV5gCJqLe4nM&vet=12ahUKEwi53KiVjdH6AhUS2HMBHbBICHIQMygAegUIARDoAQ..i&docid=d0e1BqY0U3w2CM&w=719&h=526&q=coder&ved=2ahUKEwi53KiVjdH6AhUS2HMBHbBICHIQMygAegUIARDoAQ",
+    tel: "+91-1234509876",
+    email: "thecoder@gmail.com"
+  }
+];
+
+export default contacts;
